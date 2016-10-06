959db31bd2dfbec04c297a247525682edaefa2e6
ManuelPe <manuel.pena@miurabox.com>
2016-10-06 17:29:54 -0500

    First update

---
+++ 881.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+date=`date +"%Y%m%d"`
+hour_tmp=`date +"%k"`
+url=http://ice22.securenetsystems.net/UNIVM
+duration=3600
+
+if [ $hour_tmp -lt 12 ]; then
+	hour=Am
+else
+	hour=Pm
+fi
+
+output_file=Podcast_${date}_Beatles_$hour
+output_dir=/home/Downloads
+cd $output_dir
+
+#touch $output_file
+
+streamripper $url -d $output_dir -l $duration -a $output_file -o always
